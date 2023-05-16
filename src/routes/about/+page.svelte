d3152461ad96e529e53135c53bf80b37f919b183
Libertas <1adamsvoboda@gmail.com>
2023-05-16 16:33:51 +0200

    fixed wrong link

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,8 +7,8 @@
 	<h1>About BoatCup</h1>
 
 	<p>
-		BoatCup will be a competition in programming in the <a href="github.com/Libertas007/BoatLang"
-			>Boat programming language</a
+		BoatCup will be a competition in programming in the <a
+			href="https://github.com/Libertas007/BoatLang">Boat programming language</a
 		>.
 	</p>
 	<p>For now, it's only being prepared, the new update to Boat will be released soon!</p>
